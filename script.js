158510aef68d53229848951314a150722a90f506
Sofiia <sofiiavoitkova@gmail.com>
2024-07-11 23:20:04 +0200

    feat: add initial drawing logic

---
+++ script.js
@@ -0,0 +1,27 @@
+const board = document.getElementById("game-board");
+
+let snake = [{ x: 10, y: 10 }];
+
+function draw() {
+  board.innerHTML = "";
+  drawSnake();
+}
+
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
+    setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
+  });
+}
+
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+}
