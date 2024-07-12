25df8b1d94e9ac110d22dee565261c8850c30c47
Sofiia <sofiiavoitkova@gmail.com>
2024-07-12 08:10:47 +0200

    feat: add food drawing function

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const board = document.getElementById("game-board");
 
+const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
 
 function draw() {
   board.innerHTML = "";
   drawSnake();
+  drawFood();
 }
 
 function drawSnake() {
@@ -24,4 +27,18 @@ function createGameElement(tag, className) {
 function setPosition(element, position) {
   element.style.gridColumn = position.x;
   element.style.gridRow = position.y;
+}
+
+function drawFood() {
+  if (gameStarted) {
+    const foodElement = createGameElement("div", "food");
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+  }
+}
+
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
 }
